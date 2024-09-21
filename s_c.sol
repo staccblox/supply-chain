5d0030d1813655b15d74d71accd70f6e8f232add
staccblox <67884277+staccblox@users.noreply.github.com>
2024-09-21 16:10:43 -0400

    Create s_c.sol

---
+++ s_c.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+contract s_c {
+   constructor() public {
+
+   }
+   function getResult() public view returns(uint) {
+      uint a = 1;
+      uint b = 2;
+      uint result = a + b;
+      return result;
+   }
+}
