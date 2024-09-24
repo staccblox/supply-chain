a91a265e041eb95c105666ba300039a359f5342c
staccblox <67884277+staccblox@users.noreply.github.com>
2024-09-24 13:36:16 -0400

    Update s_c.sol

--- s_c.sol
+++ s_c.sol
@@ -1,12 +1,5 @@
-pragma solidity ^0.5.0;
-contract s_c {
-   constructor() public {
+pragma solidity ^0.8.26;
 
-   }
-   function getResult() public view returns(uint) {
-      uint a = 1;
-      uint b = 2;
-      uint result = a + b;
-      return result;
-   }
+contract supply_chain {
+    string public greet = "complex logistics system that consists of facilities that convert raw materials into finished products and distribute them to end consumers or end customers. Meanwhile, supply chain management deals with the flow of goods in distribution channels within the supply chain in the most efficient manner.";
 }
