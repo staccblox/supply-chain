87fe682dff56a779c1f41edd3d30b3a819d92980
ietzel <67884277+ietzel@users.noreply.github.com>
2024-10-09 10:48:44 -0400

    Update s_c.sol

--- s_c.sol
+++ s_c.sol
@@ -2,4 +2,5 @@ pragma solidity ^0.8.26;
 
 contract supply_chain {
     string public greet = "complex logistics system that consists of facilities that convert raw materials into finished products and distribute them to end consumers or end customers. Meanwhile, supply chain management deals with the flow of goods in distribution channels within the supply chain in the most efficient manner.";
+    string public greet_two = "Try train trolley problems rather than risk assessment for a people-oriented approach while staying judgemental."
 }
